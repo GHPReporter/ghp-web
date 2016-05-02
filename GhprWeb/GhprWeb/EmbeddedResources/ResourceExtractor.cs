14b51b5302d1363917590e147a3afc39a355db36
Evgeniy Kosjakov <42_@mail.ru>
2016-05-02 14:52:43 +0300

    extracting resources added

--- GhprWeb/GhprWeb/EmbeddedResources/ResourceExtractor.cs
+++ GhprWeb/GhprWeb/EmbeddedResources/ResourceExtractor.cs
@@ -1,7 +1,39 @@
-﻿namespace GhprWeb.EmbeddedResources
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GhprWeb.EmbeddedResources
 {
     public class ResourceExtractor
     {
+        private void ExtractResource(string embeddedFileName, string destinationPath, bool replaceExisting = false)
+        {
+            var currentAssembly = GetType().Assembly;
+            var arrResources = GetType().Assembly.GetManifestResourceNames();
+            var destinationFullPath = Path.Combine(destinationPath, embeddedFileName);
+            if (!File.Exists(destinationFullPath) || replaceExisting)
+            {
+                foreach (var resourceName in arrResources
+                    .Where(resourceName => resourceName.ToUpper().EndsWith(embeddedFileName.ToUpper())))
+                {
+                    using (var resourceToSave = currentAssembly.GetManifestResourceStream(resourceName))
+                    {
+                        using (var output = File.Create(destinationFullPath))
+                        {
+                            resourceToSave?.CopyTo(output);
+                        }
+                        resourceToSave?.Close();
+                    }
+                }
+            }
+        }
 
+        private void ExtractResources(List<string> embeddedFileNames, string destinationPath, )
+        {
+            foreach (var embeddedFileName in embeddedFileNames)
+            {
+                ExtractResource(embeddedFileName, destinationPath);
+            }
+        }
     }
 }
